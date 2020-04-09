7dc27b08c2e2238a29a94807becf42cf3db03be3
Fraser Gordon <51318560+FraserG01@users.noreply.github.com>
2020-04-09 13:04:20 +0100

    cmake-fail: updated libsdk cmake to utilise CSDK_DIR (#24)
    * cmake-fail: updated libsdk cmake to utilise CSDK_DIR
    
    Signed-off-by: Fraser <fraser@iotechsys.com>

--- cmake/FindLibcsdk.cmake
+++ cmake/FindLibcsdk.cmake
@@ -1,4 +1,12 @@
-find_path(LIBCSDK_INCLUDE_DIR edgex/edgex.h edgex/devsdk.h)
-find_library(LIBCSDK_LIBRARIES NAMES csdk libcsdk)
+find_library(LIBCSDK_LIBRARIES NAMES csdk libcsdk PATHS ENV CSDK_DIR PATH_SUFFIXES lib)
+if(LIBCSDK_LIBRARIES STREQUAL "LIBCSDK_LIBRARIES-NOTFOUND")
+    message(FATAL_ERROR "No C SDK library found in the default paths or $CSDK_DIR/lib. Please check your installation.")
+endif()
+
+find_path(LIBCSDK_INCLUDE_DIR edgex/edgex.h edgex/devsdk.h PATHS ENV CSDK_DIR PATH_SUFFIXES include)
+if(LIBCSDK_INCLUDE_DIR STREQUAL "LIBCSDK_INCLUDE_DIR-NOTFOUND")
+    message(FATAL_ERROR "No C SDK header found in the default paths or $CSDK_DIR/include. Please check your installation.")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBCSDK DEFAULT_MSG LIBCSDK_LIBRARIES LIBCSDK_INCLUDE_DIR)
